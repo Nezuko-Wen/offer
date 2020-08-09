e268b766e64f4cb1370e1fbbfa90d0c1cb6b5e14
Nezuko-Wen <409053122@qq.com>
2020-08-09 13:41:52 +0800

    树类问题测试类

---
+++ src/leecode/tree/TreeNode.java
@@ -0,0 +1,39 @@
+package leecode.tree;
+
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) {
+          val = x;
+      }
+
+      public static TreeNode parseTree(Integer[] treeArr) {
+            TreeNode root = new TreeNode(treeArr[0]);
+            preorderTraversalSet(root, 0, treeArr);
+            return root;
+      }
+
+      static void preorderTraversalSet(TreeNode root, int i, Integer[] treeArr) {
+            if (2 * i + 1 < treeArr.length && treeArr[2 * i + 1] != null){
+                  root.left = new TreeNode(treeArr[2 * i + 1]);
+                  preorderTraversalSet(root.left, 2 * i + 1, treeArr);
+            }else {
+                  root.left = null;
+            }
+            if (2 * i + 2 < treeArr.length && treeArr[2 * i + 2] != null){
+                  root.right = new TreeNode(treeArr[2 * i + 2]);
+                  preorderTraversalSet(root.right, 2 * i + 2, treeArr);
+            }else {
+                  root.right = null;
+            }
+      }
+
+      public void preorderTraversalPrint(TreeNode root) {
+            if (root != null) {
+                  System.out.println(root.val);
+                  preorderTraversalPrint(root.left);
+                  preorderTraversalPrint(root.right);
+            }
+      }
+}
