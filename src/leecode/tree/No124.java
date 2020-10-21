beeb96792270b9d26873ed61c5f2b6850b62fd6b
Nezuko-Wen <409053122@qq.com>
2020-10-21 14:55:10 +0800

    优解

--- src/leecode/tree/No124.java
+++ src/leecode/tree/No124.java
@@ -4,51 +4,25 @@ package leecode.tree;
  * 二叉树的最大路径和
  */
 public class No124 {
+    private static int max = Integer.MIN_VALUE;
     public static void main(String[] args) {
-        TreeNode treeNode = TreeNode.parseTree(new Integer[]{8,9,-6,null,null,5,9});
-        TreeNode.preorderTraversalPrint(treeNode);
-        System.out.println(maxPathSum(treeNode, true));
+        TreeNode treeNode = TreeNode.parseTree(new Integer[]{-10,9,20,null,null,15,7});
+        maxPathSum(treeNode);
+        System.out.println(max);
     }
 
-    public static int maxPathSum(TreeNode root, boolean rootNode) {
-        int rootVal = root.val;
-
-        int max = rootVal;
-        if (root.left == null && root.right == null) {
-            return max;
+    public static int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
         }
-        if (rootVal < 0 && rootNode) {
-            if (root.left != null) {
-                max = Math.max(max, maxPathSum(root.left, true));
-            }
-            if (root.right != null) {
-                max = Math.max(max, maxPathSum(root.right, true));
-            }
-            return max;
-        }
-        int leftMax = 0;
-        int rightMax = 0;
-        if (root.left != null) {
-            leftMax = maxPathSum(root.left, false);
-            if (root.left.val >= 0){
-                max = Math.max(rootVal, rootVal + leftMax);
-            }
-            if (rootNode) {
-                max = Math.max(max, leftMax);
-            }
-        }
-        if (root.right != null) {
-            rightMax = maxPathSum(root.right, false);
-            if (root.right.val >= 0){
-                max = Math.max(max, rootVal + rightMax);
-            }
-            if (rootNode) {
-                max = Math.max(max, rightMax);
-            }
-        }
-        if (root.left != null && root.right != null && rootNode) {
-            max = Math.max(max, rightMax + rootVal + leftMax);
-        }
-        return max;
+        int left = maxPathSum(root.left);
+        int right = maxPathSum(root.right);
+        //单独分支产生的最大值，这种情况不可递归，用全局变量去存储，看最后会不会是这个单独分支的值最大
+        int allSum = root.val + Math.max(0, left) + Math.max(0, right);
+        //单边的最大值，将继续和父层累加
+        int sideMax = root.val + Math.max(0, Math.max(left, right));
+        max = Math.max(max, Math.max(allSum, sideMax));
+        //只把单边的最大值去做递归计算
+        return sideMax;
     }
 }
