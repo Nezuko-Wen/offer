d7e7cf9a5249bf973102e99e03395221f539ae59
Nezuko-Wen <409053122@qq.com>
2020-10-23 10:23:14 +0800

    排序

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package sort;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * 冒泡排序
+ * @param <T>
+ */
+public class BubbleSort<T extends Comparable<T>> extends Sort<T>{
+    @Override
+    public void sort(T[] nums) {
+        boolean replaced = false;
+        for (int i = 0; i < nums.length - 1; i ++){
+            for (int j = 0; j < nums.length - i - 1; j ++){
+                if (less(nums[j+1], nums[j])){
+                    swap(nums, j, j+1);
+                    replaced = true;
+                }
+            }
+            if (replaced){
+                replaced = false;
+            }else {
+                return;
+            }
+        }
+    }
+}
