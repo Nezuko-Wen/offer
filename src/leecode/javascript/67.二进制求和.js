c7eafcfc21c91a0e7748fdbf7a6783cea34e4a20
Wen <409053122@qq.com>
2023-04-21 14:21:32 +0800

    [67] 二进制求和

--- src/leecode/javascript/67.二进制求和.js
+++ src/leecode/javascript/67.二进制求和.js
@@ -10,5 +10,19 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function (a, b) {};
+var addBinary = function (a, b) {
+  let carry = 0,
+    index = 1,
+    ret = '';
+  while (a[a.length-index] || b[b.length-index]) {
+    const aval = parseInt(a[a.length-index] || 0);
+    const bval = parseInt(b[b.length-index] || 0);
+    ret = ((aval ^ bval) ^ carry) + ret;
+    carry = (aval & bval) | (aval & carry) | (bval & carry);
+    index ++;
+  }
+  if (carry) ret = carry + ret;
+  return ret;
+};
+console.log(addBinary('1', '11'));
 // @lc cod   e=end
